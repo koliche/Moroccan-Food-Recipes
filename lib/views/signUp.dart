d25b7b68097674de0e4368f98a490fa8617b05c8
Ibtissam <ibtissamchouklati2001@gmail.com>
2022-06-07 10:56:35 +0100

    commit 3

--- lib/views/signUp.dart
+++ lib/views/signUp.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/views/widgets/LoginWidget.dart';
+import 'package:recipes_app/views/widgets/pageTiteleBar.dart';
+import 'package:recipes_app/views/widgets/roundedButton.dart';
+import 'package:recipes_app/views/widgets/roundedInputField.dart';
+import 'package:recipes_app/views/widgets/roundedPasswordField.dart';
+
+import 'login.dart';
+
+
 
 class SignUpScreen extends StatefulWidget {
   SignUpScreen({Key? key}) : super(key: key);
@@ -8,18 +17,110 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _nameTextController = TextEditingController();
+ 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-          child: Text(
-        "Register Widget",
-        style: TextStyle(
-          fontSize: 25,
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: size.width,
+          height: size.height,
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                 Upside(),
+                const PageTitleBar(title: 'Create New Account'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 320.0),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        
+                        const Text(
+                          "or use your email account",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontFamily: 'OpenSans',
+                              fontSize: 13,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Form(
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(left: 10, right: 10),
+                                child: RoundedInputField(
+                                  hintText: "Email",
+                                  icon: Icons.email,
+                                  controller: _emailTextController,
+                                ),
+                              ),
+                               Container(
+                                padding: EdgeInsets.only(left: 10, right: 10),
+                                child: RoundedInputField(
+                                  hintText: "Name",
+                                  icon: Icons.person,
+                                  controller: _nameTextController,
+                                ),
+                              ),
+                               Container(
+                                padding: EdgeInsets.only(left: 10, right: 10),
+                                child: RoundedInputField(
+                                  hintText: "Name",
+                                  icon: Icons.person,
+                                  controller: _nameTextController,
+                                ),
+                              ),
+                               Container(
+                                  padding: EdgeInsets.only(left: 10, right: 10),
+                                  child: RoundedPasswordField(
+                                    controller: _passwordTextController,
+                                  )),
+                              
+                              RoundedButton(text: 'REGISTER', press: () {}),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              UnderPart(
+                                title: "Already have an account?",
+                                navigatorText: "Login here",
+                                onTap: () {
+                                  Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => const LoginScreen())
+                                  );
+                                },
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
-      )),
+      ),
     );
   }
 }
